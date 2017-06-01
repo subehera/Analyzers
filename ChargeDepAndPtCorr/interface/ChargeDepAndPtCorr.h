5602cebd7c0b70ef9afdda382bef69db17cdac4e
Maxime <mguilbau@cern.ch>
2017-06-01 19:30:46 +0200

    create interface dir

---
+++ ChargeDepAndPtCorr/interface/ChargeDepAndPtCorr.h
@@ -0,0 +1,64 @@
+// -*- Header -*-
+//
+// Package:    Analyzers/ChargeDepAndPtCorr
+// Class:      ChargeDepAndPtCorr
+// 
+/**\class ChargeDepAndPtCorr ChargeDepAndPtCorr.h Analyzers/ChargeDepAndPtCorr/interface/ChargeDepAndPtCorr.h
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Maxime Guilbaud
+//         Created:  Thu, 01 Jun 2017 16:56:11 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class ChargeDepAndPtCorr : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit ChargeDepAndPtCorr(const edm::ParameterSet&);
+      ~ChargeDepAndPtCorr();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
--- ChargeDepAndPtCorr/plugins/ChargeDepAndPtCorr.cc
+++ ChargeDepAndPtCorr/plugins/ChargeDepAndPtCorr.cc
@@ -16,51 +16,11 @@
 //
 //
 
-
 // system include files
-#include <memory>
 
 // user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "Analyzers/ChargeDepAndPtCorr/interface/ChargeDepAndPtCorr.h"
 
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
-
-// If the analyzer does not use TFileService, please remove
-// the template argument to the base class so the class inherits
-// from  edm::one::EDAnalyzer<> and also remove the line from
-// constructor "usesResource("TFileService");"
-// This will improve performance in multithreaded jobs.
-
-class ChargeDepAndPtCorr : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
-   public:
-      explicit ChargeDepAndPtCorr(const edm::ParameterSet&);
-      ~ChargeDepAndPtCorr();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
-
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      // ----------member data ---------------------------
-};
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
 
 //
 // constructors and destructor
