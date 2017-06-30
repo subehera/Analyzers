06ad1b28522034ace8a43e2ebb6a2a170943aadf
Maxime <mguilbau@cern.ch>
2017-06-30 19:44:20 +0200

    Close to working version

--- Cumulants/interface/Cumulants.h
+++ Cumulants/interface/Cumulants.h
@@ -19,6 +19,7 @@
 
 // system include files
 #include <memory>
+#include <vector>
 
 // CMSSW include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -28,6 +29,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
@@ -43,6 +45,7 @@
 
 // user include files
 #include "TH1F.h"
+#include "TH2D.h"
 #include "TTree.h"
 
 //
@@ -61,7 +64,7 @@ class Cumulants : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       ~Cumulants();
 
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-
+      int getEffNoffIndex();
 
    private:
       virtual void beginJob() override;
@@ -86,14 +89,52 @@ class Cumulants : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       edm::EDGetTokenT<reco::Centrality> centralityTags_;
       // used to access centrality bins 
       edm::EDGetTokenT<int> centralityBinTags_;
+      int cent_;
+
+      // ## multiplicity selection (Noff)
+      int noffmin_;          //minimum multiplicity of an event to be considered
+      int noffmax_;          //maximum multiplicity of an event to be considered
+      double ptnoffmin_;     //minimum pt cut to compute Noff
+      double ptnoffmax_;     //maximum pt cut to compute Noff
+      double dzdzerrornoff_; //cut on dz/dzerror of the tracks to compute Noff 
+      double d0d0errornoff_; //cut on d0/d0error of the tracks to compute Noff
+      double pterrorptnoff_; //cut on pterror/pt of the tracks to compute Noff
+      int noff_;             //ntrk offline value for a given event
 
       // ## track selection ##
-      double pTmin_; //min pt of the tracks
-      double pTmax_; //max pt of the tracks
+      double etamin_;    //min eta of the tracks
+      double etamax_;    //max eta of the tracks
+      double ptmin_;     //min pt of the tracks
+      double ptmax_;     //max pt of the tracks
+      double dzdzerror_; //cut on dz/dzerror of the tracks
+      double d0d0error_; //cut on d0/d0error of the tracks
+      double pterrorpt_; //cut on pterror/pt of the tracks
+      int mult_;         //multiplicity (Nref) in a given event
 
-      // ## harmonic
-      int harm_;
+      // ## vertex selection ##
+      double  minvz_;         //minimum z distance wrt (0,0,0) for the vertex       
+      double  maxvz_;         //maximum z distance wrt (0,0,0) for the vertex
+      double  maxrho_;        //cut on rho distance for the vertex position 
+      bool    isBVselByMult_; //sel best vertex based on vertex multiplicity (true) or sum p_T^2 (false)
+      int     nvtx_;          //number of reconstructed vertices in a given events
+      double  xBestVtx_;      //x coordinate of the best vertex
+      double  yBestVtx_;      //y coordinate of the best vertex
+      double  rhoBestVtx_;    //r coordinate of the best vertex
+      double  zBestVtx_;      //z coordinate of the best vertex
+      double  xBestVtxError_; //x coordinate error of the best vertex
+      double  yBestVtxError_; //y coordinate error of the best vertex
+      double  zBestVtxError_; //z coordinate error of the best vertex
+
+      // ## harmonic and cumulants ##
+      int harm_;     //harmonic order
+      bool cweight_; //use particle weight to correct from acc X eff
       
+      // ## file acc & eff & fake ##
+      edm::InputTag fname_;         //file name that contains acc X eff corrections
+      std::vector<int> effmultbin_; //Multiplicity binning of the correction 
+      TFile* feff_;                 //TFile that contains 2D histos (pt, eta) with eff/(1-fak) 
+      std::vector<TH2D*> heff_;     //TH2D histograms used for correction
+
       // ## histograms ##
       TH1F* hXBestVtx_;
       TH1F* hYBestVtx_;
@@ -102,14 +143,14 @@ class Cumulants : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       TH1F* hEtaTrk_;
       TH1F* hPtTrk_; 
       TH1F* hPhiTrk_;
+      TH1F* hEtaNoff_;
+      TH1F* hPtNoff_; 
+      TH1F* hPhiNoff_;
       TH1F* hEtaCTow_;
       TH1F* hEtCTow_;
       TH1F* hPhiCTow_;
       // ## ttree ##
       TTree* trEvent_;
-      int cent_;
-      int nVtx_;
-      int nTrk_;
 };
 
 //
--- Cumulants/plugins/Cumulants.cc
+++ Cumulants/plugins/Cumulants.cc
@@ -49,13 +49,62 @@ Cumulants::Cumulants(const edm::ParameterSet& iConfig) :
   //centrality
   centralityTags_(consumes<reco::Centrality>(iConfig.getParameter<edm::InputTag>("centralitySrc"))),
   centralityBinTags_(consumes<int>(iConfig.getParameter<edm::InputTag>("centralityBinSrc"))),
+  //multiplicity selection
+  noffmin_(iConfig.getUntrackedParameter<int>("noffmin")),
+  noffmax_(iConfig.getUntrackedParameter<int>("noffmax")),
+  ptnoffmin_(iConfig.getUntrackedParameter<double>("ptnoffmin")),
+  ptnoffmax_(iConfig.getUntrackedParameter<double>("ptnoffmax")),
+  dzdzerrornoff_(iConfig.getUntrackedParameter<double>("dzdzerrornoff")),
+  d0d0errornoff_(iConfig.getUntrackedParameter<double>("d0d0errornoff")),
+  pterrorptnoff_(iConfig.getUntrackedParameter<double>("pterrorptnoff")),
   //track selection
-  pTmin_(iConfig.getUntrackedParameter<double>("pTminTrk")),
-  pTmax_(iConfig.getUntrackedParameter<double>("pTmaxTrk")),
+  etamin_(iConfig.getUntrackedParameter<double>("etamin")),
+  etamax_(iConfig.getUntrackedParameter<double>("etamax")),
+  ptmin_(iConfig.getUntrackedParameter<double>("ptmin")),
+  ptmax_(iConfig.getUntrackedParameter<double>("ptmax")),
+  dzdzerror_(iConfig.getUntrackedParameter<double>("dzdzerror")),
+  d0d0error_(iConfig.getUntrackedParameter<double>("d0d0error")),
+  pterrorpt_(iConfig.getUntrackedParameter<double>("pterrorpt")),
+  //vertex selection
+  minvz_(iConfig.getUntrackedParameter<double>("minvz")),
+  maxvz_(iConfig.getUntrackedParameter<double>("maxvz")),
+  maxrho_(iConfig.getUntrackedParameter<double>("maxrho")),
+  isBVselByMult_(iConfig.getUntrackedParameter<bool>("isBVselByMult")),
+  nvtx_(iConfig.getUntrackedParameter<int>("nvtx",-1)),
+  xBestVtx_(iConfig.getUntrackedParameter<double>("xVtx",-99999)),
+  yBestVtx_(iConfig.getUntrackedParameter<double>("yVtx",-99999)),
+  rhoBestVtx_(iConfig.getUntrackedParameter<double>("rhoVtx",-99999)),
+  zBestVtx_(iConfig.getUntrackedParameter<double>("zVtx",-99999)),
+  xBestVtxError_(iConfig.getUntrackedParameter<double>("xVtxError",-99999)),
+  yBestVtxError_(iConfig.getUntrackedParameter<double>("yVtxError",-99999)),
+  zBestVtxError_(iConfig.getUntrackedParameter<double>("zVtxError",-99999)),
   //harmonic order
-  harm_(iConfig.getUntrackedParameter<int>("harm"))
+  harm_(iConfig.getUntrackedParameter<int>("harm")),
+  cweight_(iConfig.getUntrackedParameter<bool>("cweight")),
+  //file acc & eff & fake
+  fname_(iConfig.getUntrackedParameter<edm::InputTag>("fname")),
+  effmultbin_(iConfig.getUntrackedParameter< std::vector<int> >("effmultbin"))
 {
-   // Now do what ever initialization is needed
+   TString filename(fname_.label().c_str());
+   feff_ = 0x0;
+   if(cweight_ && !filename.IsNull())
+   {
+      edm::FileInPath fip(Form("Analyzers/Cumulants/data/EFF/%s",filename.Data()));
+      feff_ = new TFile(fip.fullPath().c_str(),"READ");
+
+      heff_.resize(feff_->GetNkeys());
+      if(heff_.size() != effmultbin_.size() - 1)
+      {
+         edm::LogWarning ("Inconsitent binning") << " Inconsistent binning for the acc X eff correction..."
+                                                 << " You might have wrong setting here";
+      }
+
+      for(unsigned short ik = 0; ik < heff_.size(); ++ik)
+      {
+         heff_[ik] = (TH2D*) feff_->Get(feff_->GetListOfKeys()->At(ik)->GetName());
+      }
+   }
+
    usesResource("TFileService");
    edm::Service<TFileService> fs;
    // Histograms
@@ -68,6 +117,9 @@ Cumulants::Cumulants(const edm::ParameterSet& iConfig) :
    hEtaTrk_ = fTrkHist.make<TH1F>("hEtatrk", "", 300, -3.,   3.);
    hPtTrk_  = fTrkHist.make<TH1F>("hPttrk",  "", 100,  0.,  10.);
    hPhiTrk_ = fTrkHist.make<TH1F>("hPhitrk", "", 640, -3.2,  3.2);
+   hEtaNoff_ = fTrkHist.make<TH1F>("hEtaNoff", "", 300, -3.,   3.);
+   hPtNoff_  = fTrkHist.make<TH1F>("hPtNoff",  "", 100,  0.,  10.);
+   hPhiNoff_ = fTrkHist.make<TH1F>("hPhiNoff", "", 640, -3.2,  3.2);
    TFileDirectory fCTowHist = fs->mkdir("CaloTowers");
    hEtaCTow_ = fCTowHist.make<TH1F>("hEtatow", "", 120, -6.,   6.);
    hEtCTow_  = fCTowHist.make<TH1F>("hEttow",  "", 100,  0.,  10.);
@@ -75,8 +127,9 @@ Cumulants::Cumulants(const edm::ParameterSet& iConfig) :
    // TTree
    trEvent_ = fs->make<TTree>("trEvent", "trEvent");
    trEvent_->Branch("centrality", &cent_, "centrality/I");
-   trEvent_->Branch("nVertex",    &nVtx_, "nVertex/I");
-   trEvent_->Branch("nTrk",       &nTrk_, "nTrk/I");
+   trEvent_->Branch("nVtx",       &nvtx_, "nVtx/I");
+   trEvent_->Branch("Noff",       &noff_, "Noff/I");
+   trEvent_->Branch("Mult",       &mult_, "Mult/I");
 }
 
 
@@ -117,19 +170,36 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    // Get vertex collection by token
    edm::Handle< reco::VertexCollection > vertices;
    iEvent.getByToken(vtxTags_, vertices);
+   reco::VertexCollection verticesColl = *vertices;
    if( !vertices->size() ) 
    { 
       edm::LogWarning ("Missing Collection") <<"Invalid or empty vertex collection!";
       return; 
    }
 
-   int nvtx = 0; // N valid vertex in collection
-   double xBestVtx = -999., yBestVtx = -999., rhoBestVtx = -999., zBestVtx = -999.; //Best vtx coordinates
-   double xBestVtxError = -999., yBestVtxError = -999., zBestVtxError = -999.; //Best vtx error
+   nvtx_          = 0;     //N valid vertex in collection
+   xBestVtx_      = -999.;
+   yBestVtx_      = -999.; 
+   rhoBestVtx_    = -999.; 
+   zBestVtx_      = -999.; //Best vtx coordinates
+   xBestVtxError_ = -999.; 
+   yBestVtxError_ = -999.; 
+   zBestVtxError_ = -999.; //Best vtx error
+
+   // Sort vertex collection if you want to select highest multiplicity vertex as best vertex
+   if(isBVselByMult_)
+   {
+      std::sort(verticesColl.begin(), verticesColl.end(), [](const reco::Vertex &a, const reco::Vertex &b)
+      {
+         if ( a.tracksSize() == b.tracksSize() ) 
+            return a.chi2() < b.chi2();
+         return a.tracksSize() > b.tracksSize();
+      });
+   }
 
    // Loop over vertices
-   for( reco::VertexCollection::const_iterator itVtx = vertices->begin();
-        itVtx != vertices->end();
+   for( reco::VertexCollection::const_iterator itVtx = verticesColl.begin();
+        itVtx != verticesColl.end();
         ++itVtx )
    {
         // Drop fake vertex and vertex with less than 2 tracks attached to it
@@ -146,27 +216,33 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
             // Radial vertex position                                                         
             double rho = sqrt(xVtx*xVtx + yVtx*yVtx);
             // Increase N valid vertex in the collection
-            ++nvtx;
+            ++nvtx_;
 
             //Get the first vertex as the best one (greatest sum p_{T}^{2}) 
             if( itVtx == vertices->begin() )
             {
-                xBestVtx = xVtx; 
-                yBestVtx = yVtx; 
-                zBestVtx = zVtx; 
-                xBestVtxError = xVtxError; 
-                yBestVtxError = yVtxError; 
-                zBestVtxError = zVtxError;
+                xBestVtx_ = xVtx; 
+                yBestVtx_ = yVtx; 
+                zBestVtx_ = zVtx; 
+                xBestVtxError_ = xVtxError; 
+                yBestVtxError_ = yVtxError; 
+                zBestVtxError_ = zVtxError;
 
-                rhoBestVtx = rho; 
+                rhoBestVtx_ = rho; 
             }
         }
         // Fill vtx histograms
-        hXBestVtx_  ->Fill(xBestVtx);
-        hYBestVtx_  ->Fill(yBestVtx);
-        hRhoBestVtx_->Fill(rhoBestVtx);
-        hZBestVtx_  ->Fill(zBestVtx);
+        hXBestVtx_  ->Fill(xBestVtx_);
+        hYBestVtx_  ->Fill(yBestVtx_);
+        hRhoBestVtx_->Fill(rhoBestVtx_);
+        hZBestVtx_  ->Fill(zBestVtx_);
    }
+
+   //Select event using vertex properties
+   if ( nvtx_ <= 0 )                               return;
+   if ( zBestVtx_ < minvz_ || zBestVtx_ > maxvz_ ) return;
+   if ( rhoBestVtx_ > maxrho_ )                    return;
+
 
    // ----- Track selection -----
    // Get track collection by token
@@ -178,19 +254,18 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
        return;
    }
 
-   int ntrk = 0; // N valid tracks in collection
-
-   // Loop over tracks
+   noff_ = 0; // Ntrk offline
+   // Loop over tracks to compute Noff first
    for( reco::TrackCollection::const_iterator itTrk = tracks->begin();
         itTrk != tracks->end();
         ++itTrk )
    {
        // Select tracks based on proximity to best vertex
-       math::XYZPoint bestvtx(xBestVtx,yBestVtx,zBestVtx);
+       math::XYZPoint bestvtx(xBestVtx_,yBestVtx_,zBestVtx_);
        double dzvtx    = itTrk->dz(bestvtx);
        double dxyvtx   = itTrk->dxy(bestvtx);
-       double dzerror  = sqrt(itTrk->dzError()*itTrk->dzError() + zBestVtxError*zBestVtxError);
-       double dxyerror = sqrt(itTrk->d0Error()*itTrk->d0Error() + xBestVtxError*yBestVtxError);
+       double dzerror  = sqrt(itTrk->dzError()*itTrk->dzError() + zBestVtxError_*zBestVtxError_);
+       double dxyerror = sqrt(itTrk->d0Error()*itTrk->d0Error() + xBestVtxError_*yBestVtxError_);
        double pterror  = itTrk->ptError();
        // Get eta, pt, phi and charge of the track
        double eta    = itTrk->eta();
@@ -200,22 +275,85 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
        // Select track based on quality
        if( !itTrk->quality(reco::TrackBase::highPurity) ) continue;
-       if( fabs(pterror) / pt      > 0.1 ) continue;
-       if( fabs(dzvtx / dzerror)   > 3.0 ) continue;
-       if( fabs(dxyvtx / dxyerror) > 3.0 ) continue;
+       if( fabs(pterror) / pt      > pterrorptnoff_ ) continue;
+       if( fabs(dzvtx / dzerror)   > dzdzerrornoff_ ) continue;
+       if( fabs(dxyvtx / dxyerror) > d0d0errornoff_ ) continue;
        if( pt < 0.0001 ) continue;
        if( charge == 0 ) continue;
 
 
        // Track selection for analysis
-       if(pt < pTmin_ || pt > pTmax_) continue;
+       if(eta < -2.4 || eta > 2.4)            continue;
+       if(pt < ptnoffmin_ || pt > ptnoffmax_) continue;
 
        // Increase N valid tracks
-       ++ntrk;
+       ++noff_;
+
        // Fill trk histograms
-       hEtaTrk_->Fill(eta);
-       hPtTrk_ ->Fill(pt);
-       hPhiTrk_->Fill(phi);
+       hEtaNoff_->Fill(eta);
+       hPtNoff_ ->Fill(pt);
+       hPhiNoff_->Fill(phi);
+   }
+
+   //Select event based on Ntrk offline selection
+   if( noff_ < noffmin_ || noff_ >= noffmax_) return;
+
+   mult_ = 0; // Event multiplicity
+   // Loop over tracks to compute cumulants and multiplicity
+   for( reco::TrackCollection::const_iterator itTrk = tracks->begin();
+        itTrk != tracks->end();
+        ++itTrk )
+   {
+       // Select tracks based on proximity to best vertex
+       math::XYZPoint bestvtx(xBestVtx_,yBestVtx_,zBestVtx_);
+       double dzvtx    = itTrk->dz(bestvtx);
+       double dxyvtx   = itTrk->dxy(bestvtx);
+       double dzerror  = sqrt(itTrk->dzError()*itTrk->dzError() + zBestVtxError_*zBestVtxError_);
+       double dxyerror = sqrt(itTrk->d0Error()*itTrk->d0Error() + xBestVtxError_*yBestVtxError_);
+       double pterror  = itTrk->ptError();
+       // Get eta, pt, phi and charge of the track
+       double eta    = itTrk->eta();
+       double pt     = itTrk->pt();
+       double phi    = itTrk->phi();
+       double charge = itTrk->charge();
+
+       // Select track based on quality
+       if( !itTrk->quality(reco::TrackBase::highPurity) ) continue;
+       if( fabs(pterror) / pt      > pterrorpt_ ) continue;
+       if( fabs(dzvtx / dzerror)   > dzdzerror_ ) continue;
+       if( fabs(dxyvtx / dxyerror) > d0d0error_ ) continue;
+       if( pt < 0.0001 ) continue;
+       if( charge == 0 ) continue;
+
+       // Track selection for analysis
+       if(eta < etamin_ || eta > etamax_) continue;
+       if(pt < ptmin_ || pt > ptmax_)     continue;
+
+       // Compute weights
+       double weight = 0.0;
+       if( cweight_ )
+       {
+           int idx = getEffNoffIndex();
+           if( idx < 0 )
+           {
+               edm::LogError ("Wrong index") <<"Index for efficiency correction not found";
+               return;
+           }
+
+	   if( feff_ && heff_[idx] ) 
+           { 
+              if( heff_[idx]->GetBinContent(heff_[idx]->FindBin(eta,pt)) != 0. ) 
+                 weight = 1./heff_[idx]->GetBinContent(heff_[idx]->FindBin(eta,pt));
+           }
+       }
+
+       // Increase N valid tracks
+       ++mult_;
+
+       // Fill trk histograms
+       hEtaNoff_->Fill(eta);
+       hPtNoff_ ->Fill(pt);
+       hPhiNoff_->Fill(phi);
    }
 
 
@@ -250,9 +388,7 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    // Fill TTree
    //cent_ = centBin;
    cent_ = 1;
-   nVtx_ = nvtx;
-   nTrk_ = ntrk;
-   trEvent_->Fill();
+   if(mult_ > 0) trEvent_->Fill();
 }
 
 
@@ -260,65 +396,6 @@ Cumulants::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 void 
 Cumulants::beginJob()
 {
-        //Init standard method
-        correlations::QVector qN(0, 0, false);
-        correlations::HarmonicVector hcN;
-        correlations::FromQVector *cqN;
-        hcN = correlations::HarmonicVector(8);
-        hcN[0] =  1*harm_;
-        hcN[1] = -1*harm_;
-        hcN[2] =  1*harm_;
-        hcN[3] = -1*harm_;
-        hcN[4] =  1*harm_;
-        hcN[5] = -1*harm_;
-        hcN[6] =  1*harm_;
-        hcN[7] = -1*harm_;
-        qN.resize(hcN);
-        cqN = new correlations::recurrence::FromQVector(qN);
-
-
-        //Init subset for subevent method
-        cumulant::Subset sub1(2);
-        sub1.set(0, "pt", 0.3, 3.0);
-        sub1.set(1, "eta", -2.4, 0.);
-        cumulant::Subset sub2(2);
-        sub2.set(0, "pt", 0.3, 3.0);
-        sub2.set(1, "eta", -2.4, 0.);
-        cumulant::Subset sub3(2);
-        sub3.set(0, "pt", 0.3, 3.0);
-        sub3.set(1, "eta", 0., 2.4);
-        cumulant::Subset sub4(2);
-        sub4.set(0, "pt", 0.3, 3.0);
-        sub4.set(1, "eta", 0., 2.4);
-
-        //Init 2-p sub-event method
-        cumulant::Set set2(2);
-        set2.setSubsetParams(0, sub1);
-        set2.setSubsetParams(1, sub3);
-
-        //Init 4-p sub-event method
-        cumulant::Set set4(4);
-        set4.setSubsetParams(0, sub1);
-        set4.setSubsetParams(1, sub2);
-        set4.setSubsetParams(2, sub3);
-        set4.setSubsetParams(3, sub4);
-
-        //Init 2-p method with subset
-        HarmonicVector h(2);
-        h[0] =  1*harm_;
-        h[1] = -1*harm_;
-        //cumulant::QVectorSet q(h, set2, false);
-        //cumulant::impl1::QVectorSet q(h, set2, false);
-        cumulant::impl2::QVectorSet q(h, set2, false);
-        //Init 4-p method with subset
-        HarmonicVector h4(4);
-        h4[0] =  1*harm_;
-        h4[1] =  1*harm_;
-        h4[2] = -1*harm_;
-        h4[3] = -1*harm_;
-        //cumulant::QVectorSet q4(h4, set4, false);
-        //cumulant::impl1::QVectorSet q4(h4, set4, false);
-        cumulant::impl2::QVectorSet q4(h4, set4, false);
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -329,12 +406,23 @@ Cumulants::endJob()
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
-Cumulants::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
+Cumulants::fillDescriptions(edm::ConfigurationDescriptions& descriptions) 
+{
+   //The following says we do not know what parameters are allowed so do no validation
+   // Please change this to state exactly what you do use, even if it is no parameters
+   edm::ParameterSetDescription desc;
+   desc.setUnknown();
+   descriptions.addDefault(desc);
+}
+
+int
+Cumulants::getEffNoffIndex() 
+{
+   for( int idx = 0; idx < (int) effmultbin_.size() - 1; ++idx )
+   {
+      if( noff_ >= effmultbin_[idx] && noff_ < effmultbin_[idx+1] ) return idx;
+   }
+   return -1;  
 }
 
 //define this as a plug-in
