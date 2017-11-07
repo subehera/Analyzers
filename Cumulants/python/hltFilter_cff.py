caadd539202b4799e9b8344a5aa7b6e746b90a9c
Wei <liwei810812@gmail.com>
2017-11-07 04:36:03 +0100

    Update

--- Cumulants/python/hltFilter_cff.py
+++ Cumulants/python/hltFilter_cff.py
@@ -10,25 +10,25 @@ hltMB.HLTPaths = ['HLT_L1MinimumBiasHF_OR_part*','HLT_ZeroBias_part*_v*','HLT_L1
 hltMB.andOr = cms.bool(True)
 hltMB.throw = cms.bool(False)
 
-hlt120 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hlt120.HLTPaths = ['HLT_PAFullTracks_Multiplicity120*_v*']
-hlt120.andOr = cms.bool(True)
-hlt120.throw = cms.bool(False)
+hlt60 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hlt60.HLTPaths = ['HLT_PixelTracks_Multiplicity60_v*']
+hlt60.andOr = cms.bool(True)
+hlt60.throw = cms.bool(False)
 
-hlt150 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hlt150.HLTPaths = ['HLT_PAFullTracks_Multiplicity120_v*','HLT_PAFullTracks_Multiplicity150*_v*']
-hlt150.andOr = cms.bool(True)
-hlt150.throw = cms.bool(False)
+hlt85 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hlt85.HLTPaths = ['HLT_PixelTracks_Multiplicity85_v*']
+hlt85.andOr = cms.bool(True)
+hlt85.throw = cms.bool(False)
 
-hlt185 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hlt185.HLTPaths = ['HLT_PAFullTracks_Multiplicity185*_v*']
-hlt185.andOr = cms.bool(True)
-hlt185.throw = cms.bool(False)
+hlt110 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hlt110.HLTPaths = ['HLT_PixelTracks_Multiplicity110_v*']
+hlt110.andOr = cms.bool(True)
+hlt110.throw = cms.bool(False)
 
-hlt250 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
-hlt250.HLTPaths = ['HLT_PAFullTracks_Multiplicity250_v*']
-hlt250.andOr = cms.bool(True)
-hlt250.throw = cms.bool(False)
+hlt135 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hlt135.HLTPaths = ['HLT_PixelTracks_Multiplicity135_v*']
+hlt135.andOr = cms.bool(True)
+hlt135.throw = cms.bool(False)
 
 NoScraping = cms.EDFilter("FilterOutScraping",
     applyfilter = cms.untracked.bool(True),
