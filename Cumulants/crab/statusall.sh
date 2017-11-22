9f197e45f0fbf7129e7e02a8e20d55e70b167227
Maxime <mguilbau@cern.ch>
2017-11-22 19:05:37 +0100

    adding chainer for analysis

--- Cumulants/crab/statusall.sh
+++ Cumulants/crab/statusall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+count=0
 for i in `ls -d Cumulant*/*`
+#for i in `ls -d Cumulant*/*std*`
 do
 
    echo " " 
@@ -9,8 +11,13 @@ do
    echo $i
    echo "---------------------------------------------"
    echo " " 
+   #crab status -d $i --long
    crab status -d $i
    sleep 1
    echo "---------------------------------------------"
    echo " " 
+   let count++
 done
+
+echo "######### Summary #######"
+echo "Number of task checked: $count"
