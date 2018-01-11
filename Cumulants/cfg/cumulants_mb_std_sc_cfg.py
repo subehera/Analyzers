0c46a1147cf9b0a5d83b257830fdd931244fe228
Maxime <mguilbau@cern.ch>
2018-01-11 15:30:49 +0100

    spilt jobs

---
+++ Cumulants/cfg/cumulants_mb_std_sc_cfg.py
@@ -0,0 +1,122 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Cumulants")
+
+
+# __________________ General _________________
+
+# Configure the logger
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+# Configure the number of maximum event the analyser run on in interactive mode
+# -1 == ALL
+process.maxEvents = cms.untracked.PSet( 
+    input = cms.untracked.int32(10) 
+)
+
+process.Timing = cms.Service("Timing",
+  summaryOnly = cms.untracked.bool(True),
+  useJobReport = cms.untracked.bool(False)
+)
+
+# __________________ I/O files _________________
+
+# Define the input file to run on in interactive mode
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+     'root://cms-xrd-global.cern.ch///store/hidata/HIRun2015/HIMinimumBias5/AOD/02May2016-v1/00000/002C1765-9B2E-E611-BA4B-F01FAFD5992D.root'
+    )
+)
+
+# Define output file name
+import os
+process.TFileService = cms.Service("TFileService",
+     fileName = cms.string('cumulants_std.root')
+)
+
+
+# __________________ Detector conditions _________________
+
+# Configure the Global Tag
+# Global tag contains information about detector geometry, calibration, alignement, ...
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '75X_dataRun2_PromptHI_v3', '')
+# Getting calib from GT
+#process.GlobalTag.toGet = cms.VPSet(
+#  cms.PSet(
+#    record = cms.string("HeavyIonRcd"),
+#    tag = cms.string("CentralityTable_HFtowersPlusTrunc200_EPOS5TeV_v80x01_mc"),
+#    connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
+#    label = cms.untracked.string("HFtowersPlusTruncEpos")
+#    )
+#)
+
+
+# __________________ Event selection _________________
+
+# Define the trigger selection
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+process.defaultTrigSel = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.defaultTrigSel.HLTPaths = ['HLT_HIL1Centralityext30100MinimumumBiasHF*'] # for allphysics
+process.defaultTrigSel.andOr = cms.bool(True)
+process.defaultTrigSel.throw = cms.bool(False)
+
+# Load HI event selection modules
+process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
+#process.load('Configuration.EventContent.EventContentHeavyIons_cff')
+
+#Primary vertex re-fitter
+process.load("RecoVertex.Configuration.RecoVertex_cff")
+process.myVertexSequence = process.unsortedOfflinePrimaryVertices.clone(
+  TkFilterParameters = cms.PSet(
+        algorithm=cms.string('filter'),
+        maxNormalizedChi2 = cms.double(20.0),
+        minPixelLayersWithHits=cms.int32(2),
+        minSiliconLayersWithHits = cms.int32(5),
+        maxD0Significance = cms.double(3.0),
+        minPt = cms.double(0.0),
+        trackQuality = cms.string("any")
+    ),
+    TkClusParameters = cms.PSet(
+        algorithm   = cms.string("gap"),
+        TkGapClusParameters = cms.PSet(
+            zSeparation = cms.double(1.0)
+        )
+    ),
+)
+
+#Reject beam scraping events standard pp configuration
+process.NoScraping = cms.EDFilter("FilterOutScraping",
+    applyfilter = cms.untracked.bool(True),
+    debugOn = cms.untracked.bool(False),
+    numtrack = cms.untracked.uint32(10),
+    thresh = cms.untracked.double(0.25)
+)
+
+process.primaryVertexFilter.src = cms.InputTag("myVertexSequence")
+process.primaryVertexFilter.cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2")
+process.primaryVertexFilter.filter = cms.bool(True)
+
+process.eventSelPbPb = cms.Sequence(process.myVertexSequence * process.hfCoincFilter3 * process.primaryVertexFilter)
+
+# __________________ Analyzer _________________
+
+# Load you analyzer with initial configuration
+process.load("Analyzers.Cumulants.cumulants_cff")
+process.anaSC23 = process.stdAnalysisSC23.clone()
+process.anaSC24 = process.stdAnalysisSC24.clone()
+
+process.p3 = cms.Path(
+                     process.eventSelPbPb *    # events sel
+                     process.defaultTrigSel *  # Select MB events
+                     process.anaSC23)          # Run the analyzer
+
+process.p4 = cms.Path(
+                     process.eventSelPbPb *    # events sel
+                     process.defaultTrigSel *  # Select MB events
+                     process.anaSC24)          # Run the analyzer
---
+++ Cumulants/crab/multicrab3_skim_PbPbppReco2015_mb_3sub_c2.py
@@ -0,0 +1,65 @@
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+
+    # We want to put all the CRAB project directories from the tasks we submit here into one common directory.
+    # That's why we need to set this parameter (here or above in the configuration file, it does not matter, we will not overwrite it).
+    from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+    config = config()
+
+    config.General.workArea = 'CumulantMB'
+    config.General.transferOutputs = True
+    config.General.transferLogs = False
+    config.JobType.pluginName = 'Analysis'
+    #config.JobType.maxMemoryMB = 2500
+    config.JobType.psetName = '../cfg/cumulants_mb_3sub_c2_cfg.py'
+    config.JobType.maxJobRuntimeMin = 2750
+    config.Data.unitsPerJob = 5
+    config.Data.totalUnits = -1
+    config.Data.splitting = 'LumiBased'
+    #config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+    config.Data.outLFNDirBase = '/store/group/phys_heavyions/flowcorr/SubCumu18/'
+    config.Data.lumiMask = 'Cert_262548-263757_PromptReco_HICollisions15_JSON_v2.txt'
+    config.Data.publication = False
+    config.Site.storageSite = 'T2_CH_CERN'
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+
+    config.General.requestName = 'PbPbppRereco2015_cumulants_3sub_b5_c2_v1'
+    config.Data.inputDataset = '/HIMinimumBias5/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+    config.General.requestName = 'PbPbppRereco2015_cumulants_3sub_b6_c2_v1'
+    config.Data.inputDataset = '/HIMinimumBias6/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+    config.General.requestName = 'PbPbppRereco2015_cumulants_3sub_b7_c2_v1'
+    config.Data.inputDataset = '/HIMinimumBias7/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+    config.General.requestName = 'PbPbppRereco2015_TrkOnly_cumulants_3sub_b5_c2_v1'
+    config.Data.lumiMask = 'Cert_263685-263757_PromptReco_HICollisions15_TrackerOnly_JSON.txt'
+    config.Data.inputDataset = '/HIMinimumBias5/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+    config.General.requestName = 'PbPbppRereco2015_TrkOnly_cumulants_3sub_b6_c2_v1'
+    config.Data.inputDataset = '/HIMinimumBias6/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+    config.General.requestName = 'PbPbppRereco2015_TrkOnly_cumulants_3sub_b7_c2_v1'
+    config.Data.inputDataset = '/HIMinimumBias7/HIRun2015-02May2016-v1/AOD'
+    submit(config)
+
+
